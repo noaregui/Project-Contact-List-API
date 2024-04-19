d7ddb2987ad2c859b21c3d79e18d89166c2e0e5d
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-04-19 11:40:14 +0000

    cambios

--- src/js/component/formulario.js
+++ src/js/component/formulario.js
@@ -19,85 +19,95 @@ export const Formulario = () => {
   };
 
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <form onSubmit={handleSubmit} className="formulario">
-            <legend>ADD A NEW CONTACT</legend>
-            <div className="mb-3">
-              <label htmlFor="nombreCompleto" className="form-label">
-                FULL NAME
-              </label>
-              <input
-                type="text"
-                id="nombreCompleto"
-                className="form-control"
-                placeholder="Full name"
-                onChange={(e) =>
-                  setContacto({ ...contacto, name: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                EMAIL
-              </label>
-              <input
-                type="text"
-                id="email"
-                className="form-control"
-                placeholder="Email"
-                onChange={(e) =>
-                  setContacto({ ...contacto, email: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="telefono" className="form-label">
-                Phone
-              </label>
-              <input
-                type="text"
-                id="telefono"
-                className="form-control"
-                placeholder="Phone"
-                onChange={(e) =>
-                  setContacto({ ...contacto, phone: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="direccion" className="form-label">
-                ADDRESS
-              </label>
-              <input
-                type="text"
-                id="direccion"
-                className="form-control"
-                placeholder="Address"
-                onChange={(e) =>
-                  setContacto({ ...contacto, address: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <div className="form-check">
-                <Link to="/" className="btn btn-secondary me-2">
-                  Got back to contacts
-                </Link>
+    <div className="formulario-crearContacto">
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={handleSubmit} className="formulario">
+              <legend>ADD A NEW CONTACT</legend>
+              <div className="mb-3">
+                <label htmlFor="nombreCompleto" className="form-label">
+                  FULL NAME
+                </label>
+                <input
+                  type="text"
+                  id="nombreCompleto"
+                  className="form-control"
+                  placeholder="Full name"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, name: e.target.value })
+                  }
+                />
               </div>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={(e) => handleSubmit(e)}
-            >
-              <i>S</i>
-              <i>A</i>
-              <i>V</i>
-              <i>E</i>
-            </button>
-          </form>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  EMAIL
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="form-control"
+                  placeholder="Email"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, email: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="telefono" className="form-label">
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  id="telefono"
+                  className="form-control"
+                  placeholder="Phone"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, phone: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="direccion" className="form-label">
+                  ADDRESS
+                </label>
+                <input
+                  type="text"
+                  id="direccion"
+                  className="form-control"
+                  placeholder="Address"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, address: e.target.value })
+                  }
+                />
+              </div>
+              <div className="botones-abajo">
+                <div className="mb-3">
+                  <Link to="/viewFormulario">
+                    <button className="CreateNewContact">
+                      <span className="span-mother">
+                        <span>S</span>
+                        <span>a</span>
+                        <span>v</span>
+                        <span>e</span>
+                      </span>
+                      <span className="span-mother2">
+                        <span>S</span>
+                        <span>a</span>
+                        <span>v</span>
+                        <span>e</span>
+                      </span>
+                    </button>
+                  </Link>
+                  <div className="form-check">
+                    <Link to="/" className="btn btn-secondary me-2">
+                      Got back to contacts
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- src/js/views/viewEditar.js
+++ src/js/views/viewEditar.js
@@ -22,95 +22,102 @@ export const ViewEditar = () => {
   };
 
   console.log(store.contactoSeleccionado)
-   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <form className="formulario">
-            <legend>EDIT CONTACT</legend>
-            <div className="mb-3">
-              <label htmlFor="nombreCompleto" className="form-label">
-                FULL NAME
-              </label>
-              <input
-                type="text"
-                value={contacto.name}
-                id="nombreCompleto"
-                className="form-control"
-                placeholder="Full name"
-                onChange={(e) =>
-                  setContacto({ ...contacto, name: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                EMAIL
-              </label>
-              <input
-                type="text"
-                value={contacto.email}
-                id="email"
-                className="form-control"
-                placeholder="Email"
-                onChange={(e) =>
-                  setContacto({ ...contacto, email: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="telefono" className="form-label">
-                PHONE
-              </label>
-              <input
-                type="text"
-                value={contacto.phone}
-                id="phone"
-                className="form-control"
-                placeholder="Teléfono"
-                onChange={(e) =>
-                  setContacto({ ...contacto, phone: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="direccion" className="form-label">
-                ADDRESS
-              </label>
-              <input
-                type="text"
-                value={contacto.address}
-                id="direccion"
-                className="form-control"
-                placeholder="Address"
-                onChange={(e) =>
-                  setContacto({ ...contacto, address: e.target.value })
-                }
-              />
-            </div>
-            j
-            <div className="mb-3">
-              <div className="form-check"></div>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={(e) => handleSubmit(e)}
-            >
-              <i>G</i>
-              <i>U</i>
-              <i>A</i>
-              <i>R</i>
-              <i>D</i>
-              <i>A</i>
-              <i>R</i>
-            </button>
-          </form>
+  return (
+    <div className="formulario-editarContacto">
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <form className="formulario">
+              <legend>EDIT CONTACT</legend>
+              <div className="mb-3">
+                <label htmlFor="nombreCompleto" className="form-label">
+                  FULL NAME
+                </label>
+                <input
+                  type="text"
+                  value={contacto.name}
+                  id="nombreCompleto"
+                  className="form-control"
+                  placeholder="Full name"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, name: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  EMAIL
+                </label>
+                <input
+                  type="text"
+                  value={contacto.email}
+                  id="email"
+                  className="form-control"
+                  placeholder="Email"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, email: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="telefono" className="form-label">
+                  PHONE
+                </label>
+                <input
+                  type="text"
+                  value={contacto.phone}
+                  id="phone"
+                  className="form-control"
+                  placeholder="Teléfono"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, phone: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="direccion" className="form-label">
+                  ADDRESS
+                </label>
+                <input
+                  type="text"
+                  value={contacto.address}
+                  id="direccion"
+                  className="form-control"
+                  placeholder="Address"
+                  onChange={(e) =>
+                    setContacto({ ...contacto, address: e.target.value })
+                  }
+                />
+              </div>
+              <div className="botones-abajo">
+                <div className="mb-3">
+                  <Link to="/viewFormulario">
+                    <button className="CreateNewContact">
+                      <span className="span-mother">
+                        <span>S</span>
+                        <span>a</span>
+                        <span>v</span>
+                        <span>e</span>
+                      </span>
+                      <span className="span-mother2">
+                        <span>S</span>
+                        <span>a</span>
+                        <span>v</span>
+                        <span>e</span>
+                      </span>
+                    </button>
+                  </Link>
+                  <div className="form-check">
+                    <Link to="/" className="btn btn-secondary me-2">
+                      Got back to contacts
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      <Link to="/" className="btn btn-secondary me-2">
-        Ir atrás a contactos
-      </Link>
     </div>
   );
 };
